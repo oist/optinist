952ad33fbd1837be710331afcf36b5a9a339e01c
Shogo Akiyama <akiyama_shogo@araya.org>
2021-08-19 18:51:24 +0900

    add flex layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Sample from 'components/sample'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <header className="App-header">
+        <p>Hello world</p>
+      </header> */}
+      <Sample />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
