92297ff1301f359c3f4af2c486e3a2b2a5cd4719
Shogo Akiyama <akiyama_shogo@araya.org>
2021-08-29 17:31:13 +0900

    add CORS

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import Depends, FastAPI
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
