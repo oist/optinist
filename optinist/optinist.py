40bf1bada28657ea5791b4b9180aa5b6129f7e27
Shogo Akiyama <akiyama_shogo@araya.org>
2021-09-01 13:43:43 +0900

    fix indent of python into 4

--- optinist/optinist.py
+++ optinist/optinist.py
@@ -1,32 +1,32 @@
 @app.get("/")
 async def root():
-  return {"message": "Hello World"}
+	return {"message": "Hello World"}
 
 class Optinist:
-  def __init__(self):
-    print('init optinist')
-    self._opts = None
+	def __init__(self):
+		print('init optinist')
+		self._opts = None
 
-  def set_data(self):
-    print('load data')
+	def set_data(self):
+		print('load data')
 
-  def set_params(self):
-    print('set params')
-    from algo import Caiman
-    opts_dict = Caiman.set_params()
-    print(opts_dict)
-    return opts_dict
+	def set_params(self):
+		print('set params')
+		from algo import Caiman
+		opts_dict = Caiman.set_params()
+		print(opts_dict)
+		return opts_dict
 
-  def get_params(self):
-    print('get params')
+	def get_params(self):
+		print('get params')
 
-  def set_cluster(self):
-    print('set cluster')
+	def set_cluster(self):
+		print('set cluster')
 
-  def run_motion_correct(self):
-    print('run motion correct')
+	def run_motion_correct(self):
+		print('run motion correct')
 
 
 if __name__ == '__main__':
-  optinist = Optinist()
-  optinist.set_params()
+	optinist = Optinist()
+	optinist.set_params()
