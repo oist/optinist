4fe2d92720cc9e548a428adb5bb4f060fa5d7cc5
Shogo Akiyama <akiyama_shogo@araya.org>
2021-10-22 13:21:01 +0900

    fix indent to spaces

--- optinist/wrappers/suite2p_wrapper/registration.py
+++ optinist/wrappers/suite2p_wrapper/registration.py
@@ -1,16 +1,16 @@
 def suite2p_registration(ops, opts=None):
-	import numpy as np
-	from suite2p import registration
+    import numpy as np
+    from suite2p import registration
 
-	######### REGISTRATION #########
-	refImg = ops['refImg'] if 'refImg' in ops and ops.get('force_refImg', False) else None
-	ops = registration.register_binary(ops, refImg=refImg) # register binary
+    ######### REGISTRATION #########
+    refImg = ops['refImg'] if 'refImg' in ops and ops.get('force_refImg', False) else None
+    ops = registration.register_binary(ops, refImg=refImg) # register binary
 
-	# compute metrics for registration
-	if ops.get('do_regmetrics', True) and ops['nframes']>=1500:
-		ops = registration.get_pc_metrics(ops)
+    # compute metrics for registration
+    if ops.get('do_regmetrics', True) and ops['nframes']>=1500:
+        ops = registration.get_pc_metrics(ops)
 
-	ops['images'] = ops['refImg'].astype(np.uint8)
-	ops['meanImgE'] = ops['meanImgE']
+    ops['images'] = ops['refImg'].astype(np.uint8)
+    ops['meanImgE'] = ops['meanImgE']
 
-	return ops
+    return ops
--- optinist/wrappers/suite2p_wrapper/roi.py
+++ optinist/wrappers/suite2p_wrapper/roi.py
@@ -1,36 +1,36 @@
 def suite2p_roi(ops, opts=None):
-	import numpy as np
-	from suite2p import extraction, classification, detection, ROI
+    import numpy as np
+    from suite2p import extraction, classification, detection, ROI
 
-	# ROI detection
-	classfile = classification.user_classfile
-	ops, stat = detection.detect(ops=ops, classfile=classfile)
+    # ROI detection
+    classfile = classification.user_classfile
+    ops, stat = detection.detect(ops=ops, classfile=classfile)
 
-	######## ROI EXTRACTION ##############
-	ops, stat, F, Fneu, F_chan2, Fneu_chan2 = extraction.create_masks_and_extract(ops, stat)
+    ######## ROI EXTRACTION ##############
+    ops, stat, F, Fneu, F_chan2, Fneu_chan2 = extraction.create_masks_and_extract(ops, stat)
 
-	######## ROI CLASSIFICATION ##############
-	iscell = classification.classify(stat=stat, classfile=classfile)
-	iscell = iscell[:, 0].astype(bool)
+    ######## ROI CLASSIFICATION ##############
+    iscell = classification.classify(stat=stat, classfile=classfile)
+    iscell = iscell[:, 0].astype(bool)
 
-	arrays = []
-	for i, s in enumerate(stat):
-		array = ROI(
-			ypix=s['ypix'], xpix=s['xpix'], lam=s['lam'], med=s['med'], do_crop=False
-		).to_array(Ly=ops['Ly'], Lx=ops['Lx'])
-		array *= i + 1
-		arrays.append(array)
+    arrays = []
+    for i, s in enumerate(stat):
+        array = ROI(
+            ypix=s['ypix'], xpix=s['xpix'], lam=s['lam'], med=s['med'], do_crop=False
+        ).to_array(Ly=ops['Ly'], Lx=ops['Lx'])
+        array *= i + 1
+        arrays.append(array)
 
-	im = np.stack(arrays)
-	im[im == 0] = np.nan
+    im = np.stack(arrays)
+    im[im == 0] = np.nan
 
-	ops['ROI_found'] = np.nanmax(im, axis=0)
-	ops['non_cell_roi'] = np.nanmax(im[~iscell])
-	ops['cell_roi'] = np.nanmax(im[iscell], axis=0)
-	ops['F'] = F
-	ops['Fneu'] = Fneu
+    ops['ROI_found'] = np.nanmax(im, axis=0)
+    ops['non_cell_roi'] = np.nanmax(im[~iscell])
+    ops['cell_roi'] = np.nanmax(im[iscell], axis=0)
+    ops['F'] = F
+    ops['Fneu'] = Fneu
 
-	ops['max_proj'] = ops['max_proj']
-	ops['images'] = ops['max_proj']
+    ops['max_proj'] = ops['max_proj']
+    ops['images'] = ops['max_proj']
 
-	return ops
+    return ops
