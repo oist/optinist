970a448b3ef48f983ce536e6d4d178fe284fd4cb
Shogo Akiyama <akiyama_shogo@araya.org>
2021-10-29 13:46:55 +0900

    add correlation

--- optinist/wrappers/__init__.py
+++ optinist/wrappers/__init__.py
@@ -1,6 +1,7 @@
 from .caiman_wrapper import caiman_wrapper_dict
 from .suite2p_wrapper import suite2p_wrapper_dict
 from .dummy_wrapper import dummy_wrapper_dict
+from .original_wrapper import original_wrapper_dict
 
 wrapper_dict = {}
 wrapper_dict.update(**caiman_wrapper_dict)
