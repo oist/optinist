4bea98e007a02c9114b3f8cc10d7ee61884483be
Shogo Akiyama <akiyama_shogo@araya.org>
2021-11-03 22:44:29 +0900

    heatmapを追加

--- frontend/src/components/Plot/HeatMap.tsx
+++ frontend/src/components/Plot/HeatMap.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
 
-// import PlotlyChart from 'react-plotlyjs-ts'
-
-// import React from 'react'
 import { useSelector } from 'react-redux'
 
 import PlotlyChart from 'react-plotlyjs-ts'
 
-import {
-  algoNameByIdSelector,
-  outputDataByIdSelector,
-  outputPathTypeByIdSelector,
-} from 'redux/slice/Algorithm/AlgorithmSelector'
+import { outputDataByIdSelector } from 'redux/slice/Algorithm/AlgorithmSelector'
 import { OutputPlotContext } from 'App'
 import { toOutputDataId } from 'redux/slice/Algorithm/AlgorithmUtils'
-
-// export const TimeSeries = React.memo(() => {
-//   // const { nodeId, outputKey } = React.useContext(OutputPlotContext)
-//   return <TimeSeriesImple />
-// })
 
 export const HeatMap = React.memo(() => {
   const { nodeId, outputKey } = React.useContext(OutputPlotContext)
@@ -28,13 +16,6 @@ export const HeatMap = React.memo(() => {
   if (currentOutputData == null) {
     return null
   }
-  console.log(currentOutputData.data)
-  // var arr = Object.keys(currentOutputData.data).map((key1, _) => {
-  //   Object.keys(currentOutputData.data).map((key2, _) => {
-  //     return currentOutputData.data[key1][key2]
-  //   })
-  // })
-  // console.log(arr)
 
   const data = [
     {
@@ -45,20 +26,7 @@ export const HeatMap = React.memo(() => {
       hoverongaps: false,
     },
   ]
-  console.log(data)
-  // const data = [
-  //   {
-  //     z: [
-  //       [1, 20, 30, 50, 1],
-  //       [20, 1, 60, 80, 30],
-  //       [30, 60, 1, -10, 20],
-  //     ],
-  //     x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-  //     y: ['Morning', 'Afternoon', 'Evening'],
-  //     type: 'heatmap',
-  //     hoverongaps: false,
-  //   },
-  // ]
+
   const layout = {
     title: 'dummy',
     margin: {
