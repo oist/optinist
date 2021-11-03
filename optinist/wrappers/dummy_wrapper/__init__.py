59275196efe782a7f5b9ef415affa70c8bbe7a77
Shogo Akiyama <akiyama_shogo@araya.org>
2021-11-03 22:37:38 +0900

    heatmap datawrapperを追加

--- optinist/wrappers/dummy_wrapper/__init__.py
+++ optinist/wrappers/dummy_wrapper/__init__.py
@@ -1,8 +1,9 @@
-from .dummy import dummy_image2image, dummy_image2time, dummy_time2time, dummy_image2image8time
+from .dummy import *
 
 dummy_wrapper_dict = {
 	'dummy_image2image': dummy_image2image,
 	'dummy_image2time': dummy_image2time,
+	'dummy_image2heat': dummy_image2heat,
 	'dummy_time2time': dummy_time2time,
 	'dummy_image2image8time': dummy_image2image8time
 }
