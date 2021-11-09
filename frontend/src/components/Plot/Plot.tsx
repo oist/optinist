df98fe42f88b193190dfe90dcc3087d4b4c2f560
Shogo Akiyama <akiyama_shogo@araya.org>
2021-11-09 12:55:19 +0900

    Plotlyによる画像描画を追加

--- frontend/src/components/Plot/Plot.tsx
+++ frontend/src/components/Plot/Plot.tsx
@@ -3,20 +3,22 @@ import { useSelector } from 'react-redux'
 
 import { outputPathTypeByIdSelector } from 'redux/slice/Algorithm/AlgorithmSelector'
 import { OutputPlotContext } from 'App'
+import { ImagePlot } from './ImagePlot'
 import { TimeSeries } from './TimeSeries'
 import { HeatMap } from './HeatMap'
 import { OUTPUT_TYPE_SET } from 'redux/slice/Algorithm/AlgorithmType'
 
 export const Plot = React.memo(function PlotOutput() {
-  const { nodeId, outputKey } = React.useContext(OutputPlotContext)
-  const outputType = useSelector(outputPathTypeByIdSelector(nodeId, outputKey))
-  switch (outputType) {
-    case OUTPUT_TYPE_SET.HEAT_MAP:
-      return <HeatMap />
-    case OUTPUT_TYPE_SET.TIME_SERIES:
-      return <TimeSeries />
-    case OUTPUT_TYPE_SET.IMAGE:
-    default:
-      return null
-  }
+  return <ImagePlot />
+  // const { nodeId, outputKey } = React.useContext(OutputPlotContext)
+  // const outputType = useSelector(outputPathTypeByIdSelector(nodeId, outputKey))
+  // switch (outputType) {
+  //   case OUTPUT_TYPE_SET.HEAT_MAP:
+  //     return <HeatMap />
+  //   case OUTPUT_TYPE_SET.TIME_SERIES:
+  //     return <TimeSeries />
+  //   case OUTPUT_TYPE_SET.IMAGE:
+  //   default:
+  //     return null
+  // }
 })
