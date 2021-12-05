207e2716fc90ceab44a609f4b5d91b21d0eaf677
Shogo Akiyama <akiyama_shogo@araya.org>
2021-12-05 17:47:55 +0900

    tif変換のコード乗りファクタリング

--- backend/routers/outputs.py
+++ backend/routers/outputs.py
@@ -8,15 +8,15 @@ router = APIRouter()
 @router.get("/outputs/{file_path:path}")
 async def read_file(file_path: str):
     file_path = os.path.join(".", file_path)
+    print(file_path)
 
     file_name, ext = os.path.splitext(os.path.basename(file_path))
-    folder_path = os.path.dirname(file_path)
-    print(file_path)
-    if ext == '.tif' and not os.path.exists(os.path.join(folder_path, f'{file_name}.json')):
-        save_tiff_to_json(file_path, 10)
+
+    if ext == '.tif':
+        folder_path = os.path.dirname(file_path)
         file_path = os.path.join(folder_path, f'{file_name}.json')
-    else:
-        file_path = os.path.join(folder_path, f'{file_name}.json')
+        if not os.path.exists(file_path):
+            save_tiff_to_json(file_path, 10)
 
     with open(file_path, 'r') as f:
         json_dict = json.load(f)
