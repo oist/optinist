ba267048973099d5f1acf6919d1729924cb2d9f1
ShogoAkiyama <akiyama_shogo@araya.org>
2022-02-12 16:11:56 +0900

    backend APIを作成

--- backend/routers/hdf5.py
+++ backend/routers/hdf5.py
@@ -9,25 +9,39 @@ async def get_files():
     file_path = '/tmp/optinist/20220129-2131/20220129-2131.hdf5'
 
     # print_list = {}
-    ds_dict = {}
+    ds_dict = {
+        "isDir": True,
+        "nodes": [],
+    }
     def get_ds_dictionaries(name, node):
         fullname = node.name
         if isinstance(node, h5py.Dataset):
             path_list = name.split('/')
-            dict2leaf(ds_dict, path_list)
+            get_dir_tree(ds_dict["nodes"], path_list)
 
-    def dict2leaf(ds_dict, path_list):
+    def get_dir_tree(nodes, path_list):
         name = path_list[0]
-        if len(path_list) > 1:
-            if name not in ds_dict.keys():
-                ds_dict[name] = {
+        is_exists = False
+        # 既にkeyがある
+        for i, node in enumerate(nodes):
+            if "name" in node.keys() and node["name"] == name:
+                is_exists = True
+                if len(path_list) > 1:
+                    get_dir_tree(nodes[i]["nodes"], path_list[1:])
+
+        if not is_exists:
+            if len(path_list) > 1:
+                nodes.append({
                     "isDir": True,
-                }
-            dict2leaf(ds_dict[name], path_list[1:])
-        else:
-            ds_dict[name] = {
-                "isDir": False
-            }
+                    "name": name,
+                    "nodes": [],
+                })
+                get_dir_tree(nodes[-1]["nodes"], path_list[1:])
+            else:
+                nodes.append({
+                    "isDir": False,
+                    "name": name,
+                })
 
     with h5py.File(file_path, "r") as f:
         f.visititems(get_ds_dictionaries)
