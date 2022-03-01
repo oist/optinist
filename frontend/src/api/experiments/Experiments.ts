f52bec9c5bfcb559cde4839e898330534bcab209
ShogoAkiyama <akiyama_shogo@araya.org>
2022-03-01 11:38:03 +0900

    experiment importのfrontned部分を作成途中

--- frontend/src/api/experiments/Experiments.ts
+++ frontend/src/api/experiments/Experiments.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 import { BASE_URL } from 'const/API'
+import { Elements } from 'react-flow-renderer'
+import { NodeData } from 'store/slice/FlowElement/FlowElementType'
 
 export type ExperimentsDTO = {
   [uid: string]: ExperimentDTO
@@ -37,5 +39,12 @@ export async function getExperiments(): Promise<ExperimentsDTO> {
 
 export async function deleteExperimentByUid(uid: string): Promise<boolean> {
   const response = await axios.delete(`${BASE_URL}/experiments/${uid}`)
+  return response.data
+}
+
+export async function importExperimentByUidApi(
+  uid: string,
+): Promise<Elements<NodeData>> {
+  const response = await axios.get(`${BASE_URL}/experiments/import/${uid}`)
   return response.data
 }
