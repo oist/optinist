bd0ef193a55c1ab8a4633235fa466d30aa843cce
ShogoAkiyama <akiyama_shogo@araya.org>
2022-03-02 21:31:11 +0900

    途中実行のAPIを作成

--- backend/routers/run.py
+++ backend/routers/run.py
@@ -24,15 +24,25 @@ class RunItem(BaseModel):
     nwbParam: dict = {}
 
 
-@router.post("/run")
-async def params(runItem: RunItem, background_tasks: BackgroundTasks):
-    unique_id = str(uuid.uuid4())
-
+def run_workflow(unique_id, background_tasks, runItem):
     set_workflow(unique_id, runItem)
 
     snakemake_params = get_typecheck_params(runItem.snakemakeParam, "snakemake")
     background_tasks.add_task(run_snakemake, snakemake_params)
 
+
+@router.post("/run")
+async def params(runItem: RunItem, background_tasks: BackgroundTasks):
+    unique_id = str(uuid.uuid4())
+    run_workflow(unique_id, background_tasks, runItem)
+    print("run snakemake")
+
+    return unique_id
+
+
+@router.post("/run/{uid}")
+async def params(uid: str, runItem: RunItem, background_tasks: BackgroundTasks):
+    run_workflow(uid, background_tasks, runItem)
     print("run snakemake")
 
     return unique_id
