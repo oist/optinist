1dd87059e7817ea5c650b5a9187e28e648e21e58
ShogoAkiyama <akiyama_shogo@araya.org>
2022-03-06 19:35:07 +0900

    途中実行のbackend側を修正

--- optinist/cui_api/write_config.py
+++ optinist/cui_api/write_config.py
@@ -19,18 +19,31 @@ def write_snakemake_config(unique_id, flow_config):
 
 
 def write_experiment_config(unique_id, rule_config, runItem):
-    experiment_config = {
-        "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        "name": "test",
-        "unique_id": unique_id,
-        "function": {},
-        "nodeList": runItem.nodeList,
-        "edgeList": runItem.edgeList,
-    }
+
+    save_path = join_file_path([BASE_DIR, unique_id])
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    exp_filepath = join_file_path([save_path, 'experiment.yaml'])
+
+    if os.path.exists(exp_filepath):
+        with open(exp_filepath, "r") as f:
+            experiment_config = yaml.safe_load(f)
+        experiment_config["timestamp"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        experiment_config["nodeList"] = runItem.nodeList
+        experiment_config["edgeList"] = runItem.edgeList
+    else:
+        experiment_config = {
+            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "name": runItem.name,
+            "unique_id": unique_id,
+            "function": {},
+            "nodeList": runItem.nodeList,
+            "edgeList": runItem.edgeList,
+        }
 
     for node in runItem.nodeList:
         name = node["data"]["label"]
-        # position = node["position"]
 
         success = "running"
         if node["type"] == "ImageFileNode":
@@ -38,13 +51,8 @@ def write_experiment_config(unique_id, rule_config, runItem):
 
         experiment_config["function"][name] = {
             "unique_id": node["id"],
-            # "position": position,
             "success": success,
         }
-        
-    save_path = join_file_path([BASE_DIR, unique_id])
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
 
     with open(join_file_path([save_path, 'experiment.yaml']), "w") as f:
         yaml.dump(experiment_config, f)
