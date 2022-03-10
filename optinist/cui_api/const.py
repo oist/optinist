8ff44a02ec33ae9701aaa4e11fcaf3ffdafb9e47
ShogoAkiyama <akiyama_shogo@araya.org>
2022-03-10 09:24:17 +0900

    環境変数で保存先を変えるように変更

--- optinist/cui_api/const.py
+++ optinist/cui_api/const.py
@@ -1,7 +1,12 @@
 import os
 from .utils import join_file_path
 
-BASE_DIR = '/tmp/optinist'
+_OPTINIST_DIR = os.environ.get('OPTINIST_DIR')
+if _OPTINIST_DIR is None:
+    BASE_DIR = '/tmp/optinist'
+else:
+    BASE_DIR = _OPTINIST_DIR
+
 OPTINIST_DIR = join_file_path([os.path.dirname(__file__), ".."])
 
 if not os.path.exists(BASE_DIR):
