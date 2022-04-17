0b312342a841ff2f936f906fef6a54c31ccda3d7
Yoshifumi Takeshima <>
2022-04-17 20:26:35 +0900

    リサイズ機能修正

--- frontend/src/components/Visualize/VisualizeItem.tsx
+++ frontend/src/components/Visualize/VisualizeItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import InputLabel from '@mui/material/InputLabel'
@@ -46,81 +45,106 @@ export const VisualizeItem = React.memo<{ itemId: number }>(({ itemId }) => {
   const isSelected = useSelector(
     (state: RootState) => selectSelectedVisualizeItemId(state) === itemId,
   )
-  const theme = useTheme()
-
-  const width = useSelector(selectVisualizeItemWidth(itemId))
-  const height = useSelector(selectVisualizeItemHeight(itemId))
-
-  const itemDataType = useSelector(selectVisualizeDataType(itemId))
-
-  const [resizeTrigger, setResizeTrigger] = React.useState(false)
-  const [resizeCoord, setResizeCoord] = React.useState<{
-    x: number
-    y: number
-  }>({ x: 0, y: 0 })
-
-  const onMouseDown = (event: React.MouseEvent<HTMLInputElement>) => {
-    setResizeTrigger(true)
-    setResizeCoord({ x: event.screenX, y: event.screenY })
-  }
-
-  const onMouseUp = () => {
-    setResizeTrigger(false)
-  }
-
-  const onMouseLeave = () => {
-    setResizeTrigger(false)
-  }
-
-  const onMouseMove = (event: React.MouseEvent<HTMLInputElement>) => {
-    if (resizeTrigger) {
-      const newWidth = width + (event.screenX - resizeCoord.x)
-      const newHeight = height + (event.screenY - resizeCoord.y)
-      dispatch(
-        setItemSize({
-          itemId,
-          width: newWidth,
-          height: newHeight,
-        }),
-      )
-      setResizeCoord({ x: event.screenX, y: event.screenY })
-    }
-  }
-
+  const { size, onMouseDownX, onMouseDownY, onMouseDownXY } =
+    useItemDragResize(itemId)
   return (
-    <Paper
-      variant="outlined"
-      key={itemId}
-      style={{
-        width: `${width}px`,
-        height: `${height}px`,
-        margin: theme.spacing(1),
-        padding: theme.spacing(1),
-        cursor: resizeTrigger ? 'nwse-resize' : 'pointer',
-        borderColor: isSelected ? theme.palette.primary.light : undefined,
-      }}
-      onClick={onClick}
-      onMouseDown={onMouseDown}
-      onMouseLeave={onMouseLeave}
-      onMouseUp={onMouseUp}
-      onMouseMove={onMouseMove}
-    >
-      <Box display="flex" justifyContent="flex-end">
-        <Box flexGrow={1}>
-          <>ID: {itemId}</>
-          <FilePathSelectItem itemId={itemId} />
-        </Box>
-        {itemDataType === DATA_TYPE_SET.TIME_SERIES && (
-          <Box flexGrow={1}>
-            <RefImageItemIdSelect itemId={itemId} />
-          </Box>
-        )}
-        <Box>
-          <DisplayDataItemLayoutMenuIcon itemId={itemId} />
+    <Box sx={{ m: 1, display: 'flex', flexDirection: 'row' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Paper
+          variant="outlined"
+          key={itemId}
+          onClick={onClick}
+          sx={{
+            width: `${size.width}px`,
+            height: `${size.height}px`,
+            p: 1,
+            borderColor: (theme) =>
+              isSelected ? theme.palette.primary.light : undefined,
+          }}
+        >
+          <ItemHeader itemId={itemId} />
+          <DisplayDataItem itemId={itemId} />
+        </Paper>
+        <Box
+          sx={{
+            display: 'flex',
+          }}
+        >
+          <Box
+            sx={{
+              flexGrow: 1,
+              position: 'relative',
+              top: '-2px',
+              height: '4px',
+              cursor: 'row-resize',
+            }}
+            onMouseDown={onMouseDownY}
+          />
+          <Box
+            sx={{
+              position: 'relative',
+              top: '-2px',
+              height: '4px',
+              width: '12px',
+              cursor: 'nwse-resize',
+            }}
+            onMouseDown={onMouseDownXY}
+          />
         </Box>
       </Box>
-      <DisplayDataItem itemId={itemId} />
-    </Paper>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Box
+          sx={{
+            flexGrow: 1,
+            position: 'relative',
+            left: '-2px',
+            width: '4px',
+            cursor: 'col-resize',
+          }}
+          onMouseDown={onMouseDownX}
+        />
+        <Box
+          sx={{
+            position: 'relative',
+            height: '12px',
+            width: '4px',
+            left: '-2px',
+            cursor: 'nwse-resize',
+          }}
+          onMouseDown={onMouseDownXY}
+        />
+      </Box>
+    </Box>
+  )
+})
+
+const ItemHeader = React.memo<{ itemId: number }>(({ itemId }) => {
+  const itemDataType = useSelector(selectVisualizeDataType(itemId))
+  return (
+    <Box display="flex" justifyContent="flex-end">
+      <Box flexGrow={1}>
+        <>ID: {itemId}</>
+        <FilePathSelectItem itemId={itemId} />
+      </Box>
+      {itemDataType === DATA_TYPE_SET.TIME_SERIES && (
+        <Box flexGrow={1}>
+          <RefImageItemIdSelect itemId={itemId} />
+        </Box>
+      )}
+      <Box>
+        <DisplayDataItemLayoutMenuIcon itemId={itemId} />
+      </Box>
+    </Box>
   )
 })
 
@@ -203,3 +227,108 @@ const RefImageItemIdSelect = React.memo<{ itemId: number }>(({ itemId }) => {
     </FormControl>
   )
 })
+
+const MIN_WIDTH = 200
+const MIN_HEIFHT = 150
+
+function useItemDragResize(itemId: number) {
+  const dispatch = useDispatch()
+  const width = useSelector(selectVisualizeItemWidth(itemId))
+  const height = useSelector(selectVisualizeItemHeight(itemId))
+  const [movingSize, setMovingSize] = React.useState({ width, height })
+  const onCommitSize = React.useCallback(
+    (size: { width: number; height: number }) =>
+      dispatch(setItemSize({ itemId, ...size })),
+    [dispatch, itemId],
+  )
+  const onMouseDownX = useMouseDragHandler(
+    (downEvent) => {
+      let movingX = downEvent.screenX
+      let movingWidth = movingSize.width
+      return {
+        onMouseMove: (moveEvent) => {
+          const newWidth = movingWidth + (moveEvent.screenX - movingX)
+          movingWidth = newWidth >= MIN_WIDTH ? newWidth : MIN_WIDTH
+          setMovingSize((size) => ({ ...size, width: movingWidth }))
+          movingX = moveEvent.screenX
+        },
+        onMouseUp: () => {
+          onCommitSize({ ...movingSize, width: movingWidth })
+        },
+      }
+    },
+    [movingSize, onCommitSize],
+  )
+  const onMouseDownY = useMouseDragHandler(
+    (downEvent) => {
+      let movingY = downEvent.screenY
+      let movingHeight = movingSize.height
+      return {
+        onMouseMove: (moveEvent) => {
+          const newHeight = movingHeight + (moveEvent.screenY - movingY)
+          movingHeight = newHeight >= MIN_HEIFHT ? newHeight : MIN_HEIFHT
+          setMovingSize((size) => ({ ...size, height: movingHeight }))
+          movingY = moveEvent.screenY
+        },
+        onMouseUp: () => {
+          onCommitSize({ ...movingSize, height: movingHeight })
+        },
+      }
+    },
+    [movingSize, onCommitSize],
+  )
+  const onMouseDownXY = useMouseDragHandler(
+    (downEvent) => {
+      let movingX = downEvent.screenX
+      let movingWidth = movingSize.width
+      let movingY = downEvent.screenY
+      let movingHeight = movingSize.height
+      return {
+        onMouseMove: (moveEvent) => {
+          const newWidth = movingWidth + (moveEvent.screenX - movingX)
+          movingWidth = newWidth >= MIN_WIDTH ? newWidth : MIN_WIDTH
+          const newHeight = movingHeight + (moveEvent.screenY - movingY)
+          movingHeight = newHeight >= MIN_HEIFHT ? newHeight : MIN_HEIFHT
+          setMovingSize({ width: movingWidth, height: movingHeight })
+          movingX = moveEvent.screenX
+          movingY = moveEvent.screenY
+        },
+        onMouseUp: () => {
+          onCommitSize({ width: movingWidth, height: movingHeight })
+        },
+      }
+    },
+    [movingSize, onCommitSize],
+  )
+  return {
+    size: movingSize,
+    onMouseDownX,
+    onMouseDownY,
+    onMouseDownXY,
+  }
+}
+
+function useMouseDragHandler(
+  onMouseDown: (event: React.MouseEvent) => {
+    onMouseMove: (this: Document, event: MouseEvent) => void
+    onMouseUp: (this: Document, event: MouseEvent) => void
+  },
+  dependencies: React.DependencyList,
+) {
+  return React.useCallback(
+    (event: React.MouseEvent) => {
+      const { onMouseMove, onMouseUp } = onMouseDown(event)
+      document.addEventListener('mousemove', onMouseMove)
+      document.addEventListener(
+        'mouseup',
+        (event) => {
+          document.removeEventListener('mousemove', onMouseMove)
+          onMouseUp.call(document, event)
+        },
+        { once: true },
+      )
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [dependencies],
+  )
+}
