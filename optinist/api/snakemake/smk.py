a9ef4f77047fdd4575479dd975c95dbc26fbff28
yousuke_kaneko <yousuke.kaneko11000@gmail.com>
2022-04-28 14:25:37 +0900

    クラスタ実行用にsnakemakeのruleを変換するメソッドを追加

--- optinist/api/snakemake/smk.py
+++ optinist/api/snakemake/smk.py
@@ -1,8 +1,11 @@
 
-from dataclasses import dataclass, field
+from dataclasses import asdict, dataclass, field
+import os
 from typing import Dict, List, Union
 
 from pydantic import BaseModel
+
+from optinist.api.dir_path import DIRPATH
 
 
 @dataclass
@@ -16,6 +19,46 @@ class Rule:
     nwbfile: dict = None
     hdf5Path: str = None
     path: str = None
+
+    def to_cluster_rule(self):
+        # 非クラスタマシン用のRuleインスタンスをもとに、
+        # 新たにクラスタマシン用のRuleを作成する
+
+        # inputとoutputのpathをクラスタマシン用に変換する
+        cluster_input = [self.replace_upper_directory(input) for input in self.input]
+        cluster_output = self.replace_upper_directory(self.output)
+
+        cluster_rule_arg_dict = {
+            **asdict(self),
+            "input": cluster_input,
+            "output": cluster_output,
+        }
+        cluster_rule = Rule(
+            **cluster_rule_arg_dict,
+        )
+        return cluster_rule
+
+    @classmethod
+    def replace_upper_directory(_cls, path):
+        # 非クラスタマシン用に設定されたディレクトリを
+        # クラスタ用に設定されたディレクトリに書き換える
+        acc = []
+        input_dir_checker = os.path.basename(DIRPATH.INPUT_DIR)
+        output_dir_checker = os.path.basename(DIRPATH.OUTPUT_DIR)
+        remaining = path
+
+        while True:
+            remaining, tmp = os.path.split(remaining)
+            to_be_checked = os.path.basename(remaining)
+            acc = [tmp] + acc
+            if to_be_checked == input_dir_checker:
+                result = os.path.join(DIRPATH.CLUSTER_INPUT_DIR, *acc)
+                break
+            elif to_be_checked == output_dir_checker:
+                result = os.path.join(DIRPATH.CLUSTER_OUTPUT_DIR, *acc)
+                break
+
+        return result
 
 
 @dataclass
