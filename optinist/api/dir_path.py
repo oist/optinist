90146b10fcdcde7dbb2c7deeef6f350334e4a057
yousuke_kaneko <yousuke.kaneko11000@gmail.com>
2022-04-28 14:24:54 +0900

    クラスタ実行用のpathを追加

--- optinist/api/dir_path.py
+++ optinist/api/dir_path.py
@@ -3,10 +3,20 @@ import os
 _DEFAULT_DIR = '/tmp/optinist'
 _ENV_DIR = os.environ.get('OPTINIST_DIR')
 
+_DEFAULT_CLUSTER_INPUT_DIR = '/tmp/optinist_cluster/input'
+_ENV_CLUSTER_INPUT_DIR = os.environ.get('OPTINIST_CLUSTER_INPUT_DIR')
+_DEFAULT_CLUSTER_OUTPUT_DIR = '/tmp/optinist_cluster/output'
+_ENV_CLUSTER_OUTPUT_DIR = os.environ.get('OPTINIST_CLUSTER_OUTPUT_DIR')
+
 class DIRPATH:
     OPTINIST_DIR = _DEFAULT_DIR if _ENV_DIR is None else _ENV_DIR
     INPUT_DIR = f"{OPTINIST_DIR}/input"
     OUTPUT_DIR = f"{OPTINIST_DIR}/output"
+
+    CLUSTER_INPUT_DIR = _DEFAULT_CLUSTER_INPUT_DIR\
+        if _ENV_CLUSTER_INPUT_DIR is None else _ENV_CLUSTER_INPUT_DIR
+    CLUSTER_OUTPUT_DIR = _DEFAULT_CLUSTER_OUTPUT_DIR\
+        if _ENV_CLUSTER_OUTPUT_DIR is None else _ENV_CLUSTER_OUTPUT_DIR
 
     ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
     CONFIG_DIR = f"{ROOT_DIR}/config"
@@ -22,3 +32,4 @@ class DIRPATH:
     SNAKEMAKE_FILEPATH = f"{ROOT_DIR}/Snakefile"
     EXPERIMENT_YML = "experiment.yaml"
     SNAKEMAKE_CONFIG_YML = "config.yaml"
+    SNAKEMAKE_CLUSTER_CONFIG_YML = "config_cluster.yaml"
