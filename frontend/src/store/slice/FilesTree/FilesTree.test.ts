6d653776ebe1233b925653f99afd7439a3e78721
Yoshifumi Takeshima <>
2022-05-04 21:26:18 +0900

    FilesTreeとAlgorithmListのテスト

---
+++ frontend/src/store/slice/FilesTree/FilesTree.test.ts
@@ -0,0 +1,92 @@
+import reducer, { initialState } from './FilesTreeSlice'
+import { FilesTree } from './FilesTreeType'
+import { getFilesTree } from './FilesTreeAction'
+
+describe('FilesTree', () => {
+  test(getFilesTree.fulfilled.type, () => {
+    expect(
+      reducer(
+        reducer(initialState, {
+          type: getFilesTree.pending.type,
+          payload: mockPayload,
+          meta: {
+            arg: 'image',
+            requestId: 'F0QeIMS-KV132B2q79qaz',
+            requestStatus: 'pending',
+          },
+        }),
+        {
+          type: getFilesTree.fulfilled.type,
+          payload: mockPayload,
+          meta: {
+            arg: 'image',
+            requestId: 'F0QeIMS-KV132B2q79qaz',
+            requestStatus: 'fulfilled',
+          },
+        },
+      ),
+    ).toEqual(expectState)
+  })
+})
+
+const mockPayload = [
+  {
+    path: '/tmp/optinist/input/hoge',
+    name: 'hoge',
+    isdir: true,
+    nodes: [
+      {
+        path: '/tmp/optinist/input/hoge/hoge.tif',
+        name: 'hoge.tif',
+        isdir: false,
+        nodes: [],
+      },
+    ],
+  },
+  {
+    path: '/tmp/optinist/input/copy_image1',
+    name: 'copy_image1',
+    isdir: true,
+    nodes: [
+      {
+        path: '/tmp/optinist/input/copy_image1/copy_image1.tif',
+        name: 'copy_image1.tif',
+        isdir: false,
+        nodes: [],
+      },
+    ],
+  },
+]
+
+const expectState: FilesTree = {
+  image: {
+    isLoading: false,
+    isLatest: true,
+    tree: [
+      {
+        path: '/tmp/optinist/input/hoge',
+        name: 'hoge',
+        isDir: true,
+        nodes: [
+          {
+            path: '/tmp/optinist/input/hoge/hoge.tif',
+            name: 'hoge.tif',
+            isDir: false,
+          },
+        ],
+      },
+      {
+        path: '/tmp/optinist/input/copy_image1',
+        name: 'copy_image1',
+        isDir: true,
+        nodes: [
+          {
+            path: '/tmp/optinist/input/copy_image1/copy_image1.tif',
+            name: 'copy_image1.tif',
+            isDir: false,
+          },
+        ],
+      },
+    ],
+  },
+}
