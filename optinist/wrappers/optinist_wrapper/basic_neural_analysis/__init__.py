f0d34ddf882790590180f49052fa36570a31d3c6
ShogoAkiyama <akiyama_shogo@araya.org>
2022-05-22 17:03:40 +0900

    不要なコードをコメント

--- optinist/wrappers/optinist_wrapper/basic_neural_analysis/__init__.py
+++ optinist/wrappers/optinist_wrapper/basic_neural_analysis/__init__.py
@@ -1,12 +1,12 @@
 from .eta import ETA
-from .cell_grouping import cell_grouping
+# from .cell_grouping import cell_grouping
 
 
 basic_neural_analysis_wrapper_dict = {
     'eta': {
         'function': ETA
     },
-    'cell_grouping': {
-        'function': cell_grouping
-    }
+    # 'cell_grouping': {
+    #     'function': cell_grouping
+    # }
 }
