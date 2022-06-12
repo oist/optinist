a6185bba7752006aa3dd4dced40274de9d191e50
ShogoAkiyama <akiyama_shogo@araya.org>
2022-06-12 15:49:16 +0900

    experimentのtestを追加

--- optinist/api/experiment/experiment_writer.py
+++ optinist/api/experiment/experiment_writer.py
@@ -17,11 +17,11 @@ class ExptConfigWriter:
         exp_filepath = join_filepath([DIRPATH.OUTPUT_DIR, unique_id, DIRPATH.EXPERIMENT_YML])
         if os.path.exists(exp_filepath):
             exp_config = ExptConfigReader.read(exp_filepath)
-            exp_config = _add_run_info(exp_config, nodeDict, edgeDict)
+            exp_config = cls.add_run_info(exp_config, nodeDict, edgeDict)
         else:
-            exp_config = _create_config(unique_id, name, nodeDict, edgeDict)
+            exp_config = cls.config(unique_id, name, nodeDict, edgeDict)
 
-        exp_config.function = _create_function_from_nodeDict(nodeDict)
+        exp_config.function = cls.function_from_nodeDict(nodeDict)
 
         ConfigWriter.write(
             dirname=join_filepath([DIRPATH.OUTPUT_DIR, unique_id]),
@@ -29,43 +29,37 @@ class ExptConfigWriter:
             config=asdict(exp_config),
         )
 
-
-def _create_config(unique_id, name, nodeDict, edgeDict) -> ExptConfig:
-    return ExptConfig(
-        unique_id=unique_id,
-        name=name,
-        timestamp=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        hasNWB=False,
-        nodeDict=nodeDict,
-        edgeDict=edgeDict,
-        function={},
-    )
-
-
-def _add_run_info(expt_config: ExptConfig, nodeDict: Dict[str, Node], edgeDict: Dict[str, Edge]) -> ExptConfig:
-    expt_config.timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    # # 関数を追加の可能性
-    expt_config.nodeDict = nodeDict
-    expt_config.edgeDict = edgeDict
-
-    for node in nodeDict.values():
-        expt_config.function[node.id] = ExptFunction(
-            unique_id=node.id,
-            name=node.data.label,
-            success="success" if node.data.type == "input" else "running",
+    @classmethod
+    def config(cls, unique_id, name, nodeDict, edgeDict) -> ExptConfig:
+        return ExptConfig(
+            unique_id=unique_id,
+            name=name,
+            timestamp=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             hasNWB=False,
+            nodeDict=nodeDict,
+            edgeDict=edgeDict,
+            function={},
         )
-    return expt_config
 
+    @classmethod
+    def add_run_info(cls, expt_config: ExptConfig, nodeDict: Dict[str, Node], edgeDict: Dict[str, Edge]) -> ExptConfig:
+        # 時間を更新
+        expt_config.timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-def _create_function_from_nodeDict(nodeDict: Dict[str, Node]) -> Dict[str, ExptFunction]:
-    return {
-        node.id: ExptFunction(
-            unique_id=node.id,
-            name=node.data.label,
-            success="success" if node.data.type == "input" else "running",
-            hasNWB=False,
-        )
-        for node in nodeDict.values()
-    }
+        # # 関数を追加の可能性
+        expt_config.nodeDict = nodeDict
+        expt_config.edgeDict = edgeDict
+
+        return expt_config
+
+    @classmethod
+    def function_from_nodeDict(cls, nodeDict: Dict[str, Node]) -> Dict[str, ExptFunction]:
+        return {
+            node.id: ExptFunction(
+                unique_id=node.id,
+                name=node.data.label,
+                success="success" if node.data.type == "input" else "running",
+                hasNWB=False,
+            )
+            for node in nodeDict.values()
+        }
