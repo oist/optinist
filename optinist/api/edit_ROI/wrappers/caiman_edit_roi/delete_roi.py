41d1f5ac9fdac94af1d13b0da77dd76fb60b36ef
quanpython <quannd@reactplus.jp>
2023-04-04 17:05:28 +0700

    fix display edit ROI caiman

--- optinist/api/edit_ROI/wrappers/caiman_edit_roi/delete_roi.py
+++ optinist/api/edit_ROI/wrappers/caiman_edit_roi/delete_roi.py
@@ -1,56 +1,35 @@
 from optinist.api.dataclass.dataclass import *
-from optinist.wrappers.caiman_wrapper.cnmf import get_roi
-
 
 
 def excute_delete_roi(node_dirpath, ids):
     import numpy as np
-    
-    from .utils import get_roi_manual
     from scipy.sparse import csc_matrix
 
-    estimates = np.load(f'{node_dirpath}/caiman_cnmf.npy', allow_pickle=True).item()
+    from .utils import get_roi
 
+    # load data
+    estimates = np.load(f'{node_dirpath}/caiman_cnmf.npy', allow_pickle=True).item()
     dims = estimates['dims']
     A = estimates.get('A').toarray()
-    A_manual = estimates.get('A_manual', np.empty((dims[0]*dims[1], 0)))
+    ims = estimates.get('ims')
+    images = estimates.get('images')
 
-    # get deleting ROI IDs
-    deleting_ROI_IDs = []
-    deleting_ROIs_manual_ID = []
-    [deleting_ROI_IDs.append(id) for id in ids if id < A.shape[-1]]
-    [deleting_ROIs_manual_ID.append(id - A.shape[-1]) for id in ids if id >= A.shape[-1]]
+    # delete ROI
+    A = np.delete(A, ids, axis=1)
+    ims = np.delete(ims, ids, axis=0)
     
-    # delete deleting ROIs from A and A_manual
-    A = np.delete(A, deleting_ROI_IDs, axis=1)
-    A_manual = np.delete(A_manual, deleting_ROIs_manual_ID, axis=1)
-
     # delete fluorescence
     estimates['C'] = np.delete(estimates['C'], ids, axis=0)
 
-    thr = 0.9
-    thr_method = 'nrg'
-    swap_dim = False
-
-    A = csc_matrix(A)
-    cell_roi = get_roi(A, thr, thr_method, swap_dim, dims)
-    start_idx = A.shape[1]
-    if A_manual.shape[1] > 0:
-        cell_roi = cell_roi + (get_roi_manual(A_manual, dims, start_idx))
-    if len(cell_roi) == 0:
-        cell_roi = np.zeros(dims)
-    else:
-        cell_roi = np.stack(cell_roi)
-        cell_roi = np.nanmax(cell_roi, axis=0).astype(float)
-    cell_roi[cell_roi == 0] = np.nan
-
+    cell_roi = get_roi(ims)
+    
     fluorescence = np.concatenate([
         estimates['C'],
         estimates['f'],
     ])
 
-    estimates['A'] = A
-    estimates['A_manual'] = A_manual
+    estimates['A'] = csc_matrix(A)
+    estimates['ims'] = ims
 
     info = {
         'fluorescence': FluoData(fluorescence, file_name='fluorescence'),
--- optinist/api/edit_ROI/wrappers/caiman_edit_roi/merge_roi.py
+++ optinist/api/edit_ROI/wrappers/caiman_edit_roi/merge_roi.py
@@ -1,68 +1,46 @@
 from optinist.api.dataclass.dataclass import *
-from optinist.wrappers.caiman_wrapper.cnmf import get_roi
 
 
 def execute_merge_roi(node_dirpath: str, ids: list):
     import numpy as np
     from scipy.sparse import csc_matrix
 
-    from .utils import get_roi_manual
+    from .utils import get_roi
 
+    # load data
     estimates = np.load(f'{node_dirpath}/caiman_cnmf.npy', allow_pickle=True).item()
-
     dims = estimates['dims']
     A = estimates.get('A').toarray()
-    A_manual = estimates.get('A_manual', np.empty((dims[0]*dims[1], 0)))
+    ims = estimates.get('ims')
+    images = estimates.get('images')
 
     # get merging ROI
     merging_ROIs = []
-    merging_ROIs_manual = []
-    [merging_ROIs.append(A[:, id]) for id in ids if id < A.shape[-1]]
-    [merging_ROIs_manual.append(A_manual[:, id - A.shape[-1]]) for id in ids if id >= A.shape[-1]]
+    [merging_ROIs.append(ims[id,:,:]) for id in ids]
 
-    # get merging ROI ID
-    merging_ROI_IDs = []
-    merging_ROIs_manual_ID = []
-    [merging_ROI_IDs.append(id) for id in ids if id < A.shape[-1]]
-    [merging_ROIs_manual_ID.append(id - A.shape[-1]) for id in ids if id >= A.shape[-1]]
+    # delete merging ROIs from A and ims
+    A = np.delete(A, ids, axis=1)
+    ims = np.delete(ims, ids, axis=0)
     
-    # delete merging ROIs from A and A_manual
-    A = np.delete(A, merging_ROI_IDs, axis=1)
-    A_manual = np.delete(A_manual, merging_ROIs_manual_ID, axis=1)
-
-    # get merged ROI
-    merged_ROI = np.maximum(*(merging_ROIs + merging_ROIs_manual))
-    merged_ROI = merged_ROI.reshape(-1, 1)
-    if not merging_ROIs_manual:
-        A = np.hstack((A, merged_ROI))
-    else:
-        A_manual = np.hstack((A_manual, merged_ROI))
+    # get merged ROI and append to A and ims
+    merged_ROI = np.maximum.reduce(merging_ROIs)
+    A = np.hstack((A, merged_ROI.reshape(-1, 1)))
+    ims = np.concatenate((ims, merged_ROI[np.newaxis, :, :]), axis=0)
 
     # get merged F
     merged_f = np.mean((estimates['C'][ids, :]), axis=0)
     estimates['C'] = np.delete(estimates['C'], ids, axis=0)
     estimates['C'] = np.vstack([estimates['C'], merged_f])
 
-    thr = 0.9
-    thr_method = 'nrg'
-    swap_dim = False
-
-    A = csc_matrix(A)
-    cell_roi = get_roi(A, thr, thr_method, swap_dim, dims)
-    start_idx = A.shape[1]
-    if A_manual.shape[1] > 0:
-        cell_roi = cell_roi + (get_roi_manual(A_manual, dims, start_idx))
-    cell_roi = np.stack(cell_roi)
-    cell_roi = np.nanmax(cell_roi, axis=0).astype(float)
-    cell_roi[cell_roi == 0] = np.nan
+    cell_roi = get_roi(ims)
 
     fluorescence = np.concatenate([
         estimates['C'],
         estimates['f'],
     ])
 
-    estimates['A'] = A
-    estimates['A_manual'] = A_manual
+    estimates['A'] = csc_matrix(A)
+    estimates['ims'] = ims
 
     info = {
         'fluorescence': FluoData(fluorescence, file_name='fluorescence'),
