e767e37801efd77aa30e15857603f25179f9f148
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-06-27 19:37:21 +0900

    adjust tests for merged handlers

--- studio/tests/core/utils/test_pickle_handler.py
+++ studio/tests/core/utils/test_pickle_handler.py
@@ -1,7 +1,7 @@
 import os
 
 from studio.core.dir_path import DIRPATH
-from studio.core.utils.pickle_handler import PickleWriter
+from studio.core.utils.pickle_handler import PickleReader, PickleWriter
 
 filepath = f"{DIRPATH.OPTINIST_DIR}/output/0123/func2/func2.pkl"
 
@@ -13,3 +13,12 @@ def test_PickleWriter():
     )
 
     assert os.path.exists(filepath)
+
+
+filepath = f"{DIRPATH.OPTINIST_DIR}/output_test/0123/func1/func1.pkl"
+
+
+def test_PickleReader():
+    data = PickleReader.read(filepath)
+
+    assert data == "abc"
