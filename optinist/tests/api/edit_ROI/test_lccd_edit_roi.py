2d9d27d3aa1fbcfd04ccbc413b61716e44423f4e
quanpython <quannd@reactplus.jp>
2023-06-27 13:43:15 +0700

    test: add lccd edit roi test

---
+++ optinist/tests/api/edit_ROI/test_lccd_edit_roi.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+from glob import glob
+
+from optinist.api.dir_path import DIRPATH
+from optinist.api.edit_ROI import ACTION, EditROI
+from optinist.api.utils.filepath_creater import join_filepath
+
+shutil.rmtree(f"{DIRPATH.OUTPUT_DIR}/lccd", ignore_errors=True)
+shutil.copytree(
+    f"{DIRPATH.OPTINIST_DIR}/output_test/lccd",
+    f"{DIRPATH.OUTPUT_DIR}/lccd",
+    dirs_exist_ok=True,
+)
+file_path = f"{DIRPATH.OUTPUT_DIR}/lccd/lccd_cell_detection/cell_roi.json"
+node_dirpath = os.path.dirname(file_path)
+
+cur_num_rois = lambda: len(  # noqa: E731
+    glob(join_filepath(f'{node_dirpath}/fluorescence/*.json'))
+)
+
+
+def test_lccd_add_roi():
+    cur_roi = cur_num_rois()
+    EditROI(
+        action=ACTION.ADD,
+        filepath=file_path,
+        params=dict(
+            posx=40,
+            posy=40,
+            sizex=10,
+            sizey=10,
+        ),
+    ).excute()
+    assert cur_num_rois() == cur_roi + 1
+
+
+def test_lccd_merge_roi():
+    cur_roi = cur_num_rois()
+    EditROI(
+        action=ACTION.MERGE,
+        filepath=file_path,
+        params=dict(ids=[1, 2, 3]),
+    ).excute()
+
+    assert cur_num_rois() == cur_roi + 1
+
+
+def test_lccd_delete_roi():
+    cur_roi = cur_num_rois()
+    EditROI(
+        action=ACTION.DELETE,
+        filepath=file_path,
+        params=dict(ids=[4, 5, 6]),
+    ).excute()
+
+    assert cur_num_rois() == cur_roi
