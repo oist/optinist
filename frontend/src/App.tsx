a350f56ebdfcf9b460fe43e58ba3bee26065e073
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-10-04 15:02:11 +0900

    restore snackbar provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import IconButton from '@mui/material/IconButton'
 import Close from '@mui/icons-material/Close'
-import { SnackbarProvider, SnackbarKey, useSnackbar, SnackbarProviderProps } from 'notistack'
+import { SnackbarProvider, SnackbarKey, useSnackbar } from 'notistack'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Layout from 'components/Layout'
 import Dashboard from 'pages/Dashboard'
@@ -14,11 +14,9 @@ import Workspaces from 'pages/Workspace'
 import Workspace from 'pages/Workspace/Workspace'
 import { IS_STANDALONE } from 'const/Mode'
 
-const SnackbarProviderComponent = SnackbarProvider as unknown as  React.FC<SnackbarProviderProps & {children: ReactNode}>
-
 const App: React.FC = () => {
   return (
-    <SnackbarProviderComponent
+    <SnackbarProvider
       maxSnack={5}
       action={(snackbarKey) => (
         <SnackbarCloseButton snackbarKey={snackbarKey} />
@@ -50,7 +48,7 @@ const App: React.FC = () => {
           )}
         </Layout>
       </BrowserRouter>
-    </SnackbarProviderComponent>
+    </SnackbarProvider>
   )
 }
 
