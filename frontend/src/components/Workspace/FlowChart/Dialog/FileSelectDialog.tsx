68183989d3039d65a98a9dbb91efd674543fdfbc
SangLv <sanglv@reactplus.com>
2023-11-22 14:15:41 +0700

    fix button action popup select file

--- frontend/src/components/Workspace/FlowChart/Dialog/FileSelectDialog.tsx
+++ frontend/src/components/Workspace/FlowChart/Dialog/FileSelectDialog.tsx
@@ -102,10 +102,10 @@ export const FileSelectDialog = memo(function FileSelectDialog({
         <FilePathSelectedListView path={selectedFilePath} />
       </DialogContent>
       <DialogActions>
-        <Button onClick={onCancel} variant="outlined" color="inherit">
+        <Button onClick={onCancel} variant="outlined">
           cancel
         </Button>
-        <Button onClick={onOk} color="primary" variant="outlined">
+        <Button onClick={onOk} variant="contained">
           OK
         </Button>
       </DialogActions>
