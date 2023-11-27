2613d4d5839b7931e3aff86109aa5944c0b1c53d
SangLv <sanglv@reactplus.com>
2023-11-27 16:09:19 +0700

    remove retry call api in getModeStandaloneApi

--- frontend/src/api/modeStandalone/index.ts
+++ frontend/src/api/modeStandalone/index.ts
@@ -2,8 +2,5 @@ import axios from "utils/axios"
 
 export const getModeStandaloneApi = async (): Promise<boolean> => {
   const res = await axios.get("/is_standalone")
-  if (res.data === undefined) {
-    return getModeStandaloneApi()
-  }
-  return !!res.data
+  return res.data
 }
