53ff150553d17e07ceaf0e6afdd8c5c0d5d2eb5e
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-12-01 15:25:27 +0900

    use menu for documentation

--- frontend/src/components/Layout/Tooltips.tsx
+++ frontend/src/components/Layout/Tooltips.tsx
@@ -1,32 +1,91 @@
-import { FC } from "react"
+import { FC, MouseEvent, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
-import GitHubIcon from "@mui/icons-material/GitHub"
-import MenuBookIcon from "@mui/icons-material/MenuBook"
-import { Tooltip } from "@mui/material"
+import { useSnackbar } from "notistack"
+
+import { Addchart, GitHub, MenuBook, OpenInNew } from "@mui/icons-material"
+import {
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from "@mui/material"
 import IconButton from "@mui/material/IconButton"
 
-import { ImportSampleDataButton } from "components/common/SampleData"
+import { getExperiments } from "store/slice/Experiments/ExperimentsActions"
+import { reset } from "store/slice/VisualizeItem/VisualizeItemSlice"
+import { importSampleData } from "store/slice/Workflow/WorkflowActions"
+import { selectCurrentWorkspaceId } from "store/slice/Workspace/WorkspaceSelector"
+import { AppDispatch } from "store/store"
 
 const Tooltips: FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const menuId = "documentation-menu"
+  const open = Boolean(anchorEl)
+  const handleClickMenuIcon = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  const handleGoToDocClick = () => {
+    window.open("https://optinist.readthedocs.io/en/latest/", "_blank")
+  }
+
+  const dispatch: AppDispatch = useDispatch()
+  const { enqueueSnackbar } = useSnackbar()
+  const workspaceId = useSelector(selectCurrentWorkspaceId)
+  const handleImportSampleDataClick = () => {
+    if (typeof workspaceId === "number") {
+      dispatch(importSampleData({ workspaceId }))
+        .unwrap()
+        .then(() => {
+          enqueueSnackbar("Sample data import success", { variant: "success" })
+          dispatch(reset())
+          dispatch(getExperiments())
+        })
+        .catch(() => {
+          enqueueSnackbar("Sample data import error", { variant: "error" })
+        })
+    }
+  }
+
   return (
     <>
-      <ImportSampleDataButton />
       <Tooltip title="GitHub repository">
-        <IconButton
-          href="https://github.com/oist/optinist"
-          target="_blank"
-        >
-          <GitHubIcon />
+        <IconButton href="https://github.com/oist/optinist" target="_blank">
+          <GitHub />
         </IconButton>
       </Tooltip>
       <Tooltip title="Documentation">
-        <IconButton
-          href="https://optinist.readthedocs.io/en/latest/"
-          target="_blank"
-        >
-          <MenuBookIcon />
+        <IconButton onClick={handleClickMenuIcon}>
+          <MenuBook />
         </IconButton>
       </Tooltip>
+      <Menu
+        id={menuId}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": menuId,
+          role: "listbox",
+        }}
+      >
+        <MenuItem onClick={handleGoToDocClick}>
+          <ListItemIcon>
+            <OpenInNew />
+          </ListItemIcon>
+          <ListItemText>Go to documentation page</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleImportSampleDataClick}>
+          <ListItemIcon>
+            <Addchart />
+          </ListItemIcon>
+          <ListItemText>Import sample data</ListItemText>
+        </MenuItem>
+      </Menu>
     </>
   )
 }
