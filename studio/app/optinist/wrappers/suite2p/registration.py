2f6bd945ae42e6dd8b688d57381b30544ee80afb
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-12-11 20:37:52 +0900

    add motion corrected image to suite2p result

--- studio/app/optinist/wrappers/suite2p/registration.py
+++ studio/app/optinist/wrappers/suite2p/registration.py
@@ -4,8 +4,8 @@ from studio.app.optinist.dataclass import Suite2pData
 
 def suite2p_registration(
     ops: Suite2pData, output_dir: str, params: dict = None, **kwargs
-) -> dict(ops=Suite2pData):
-    from suite2p import default_ops, registration
+) -> dict(ops=Suite2pData, mc_images=ImageData):
+    from suite2p import default_ops, io, registration
 
     function_id = output_dir.split("/")[-1]
     print("start suite2p registration:", function_id)
@@ -27,11 +27,14 @@ def suite2p_registration(
     if ops.get("do_regmetrics", True) and ops["nframes"] >= 1500:
         ops = registration.get_pc_metrics(ops)
 
+    mv = io.BinaryFile(Lx=ops["Lx"], Ly=ops["Ly"], read_filename=ops["reg_file"]).data
+
     info = {
         "refImg": ImageData(ops["refImg"], output_dir=output_dir, file_name="refImg"),
         "meanImgE": ImageData(
             ops["meanImgE"], output_dir=output_dir, file_name="meanImgE"
         ),
+        "mc_images": ImageData(mv, output_dir=output_dir, file_name="mc_images"),
         "ops": Suite2pData(ops, file_name="ops"),
     }
 
