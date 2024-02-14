fbe8ad6c16277a691cae68bf201418e33e51a8cb
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2024-02-14 21:38:43 +0900

    transpose fluo data

--- studio/app/optinist/wrappers/optinist/visualize_utils/fluo_from_hdf5.py
+++ studio/app/optinist/wrappers/optinist/visualize_utils/fluo_from_hdf5.py
@@ -4,6 +4,8 @@ from studio.app.optinist.dataclass.fluo import FluoData
 def fluo_from_hdf5(
     fluo: FluoData, output_dir: str, params: dict = None, **kwargs
 ) -> dict(fluorescence=FluoData):
+    import numpy as np
+
     return {
-        "fluorescence": FluoData(fluo.data, file_name="fluorescence"),
+        "fluorescence": FluoData(np.transpose(fluo.data), file_name="fluorescence"),
     }
