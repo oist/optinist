d63b19bc4e82946b2e2b08a97e0939afde041055
SangLv <sanglv@reactplus.com>
2024-02-27 17:33:32 +0700

    fix base url

--- frontend/src/const/API.ts
+++ frontend/src/const/API.ts
@@ -2,4 +2,5 @@ const HOST = process.env.REACT_APP_SERVER_HOST ?? "localhost"
 const PORT = process.env.REACT_APP_SERVER_PORT ?? 8000
 const PROTO = process.env.REACT_APP_SERVER_PROTO ?? "http"
 
-export const BASE_URL = "https://i-aa-barebone-api.whitedot.jp"
+export const BASE_URL =
+  PORT == null ? `${PROTO}://${HOST}` : `${PROTO}://${HOST}:${PORT}`
