ec52a86f8f1006c9ed6e81f6d37758fa0cbe9229
tienday <develop@itutu.local>
2024-08-02 11:04:38 +0900

    support dynamic import of .versions.json

--- frontend/src/components/common/DevelopmentInformation.tsx
+++ frontend/src/components/common/DevelopmentInformation.tsx
@@ -1,18 +1,53 @@
-import { FC, memo } from "react"
+import { FC, memo, useState, useEffect } from "react"
 
 import { Typography, Grid } from "@mui/material"
 
-import versions from ".versions.json"
 import { ParamSection } from "components/common/ParamSection"
 
+interface Versions {
+  version: string
+  nodeVersion: string
+  reactVersion: string
+}
 export const DevelopmentInformation: FC = () => {
+  const [versions, setVersions] = useState<Versions | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Note:
+        // In consideration of the case where .versions.json does not exist,
+        // dynamic import is performed using require.
+        //
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const versionsJson = require(".versions.json") as Versions
+
+        setVersions(versionsJson)
+      } catch (err) {
+        // set empty values
+        setVersions({ version: "-", nodeVersion: "-", reactVersion: "-" })
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <>
       <ParamSection title="Development Info &#x2699;">
         <Grid container mb={2}>
-          <LabelValueGridRow label="Studio ver." value={versions.version} />
-          <LabelValueGridRow label="Node ver." value={versions.nodeVersion} />
-          <LabelValueGridRow label="React ver." value={versions.reactVersion} />
+          <LabelValueGridRow
+            label="Studio ver."
+            value={versions?.version || ""}
+          />
+          <LabelValueGridRow
+            label="Node ver."
+            value={versions?.nodeVersion || ""}
+          />
+          <LabelValueGridRow
+            label="React ver."
+            value={versions?.reactVersion || ""}
+          />
         </Grid>
       </ParamSection>
     </>
