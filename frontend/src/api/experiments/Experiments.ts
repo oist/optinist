426d864dfcee6756feb21d63f7d638ca23718788
tienday <develop@itutu.local>
2024-10-12 09:01:18 +0900

    improve api request error handling

--- frontend/src/api/experiments/Experiments.ts
+++ frontend/src/api/experiments/Experiments.ts
@@ -96,7 +96,7 @@ export async function downloadExperimentConfigApi(
   return response.data
 }
 
-export async function renameExperiment(
+export async function renameExperimentApi(
   workspaceId: number,
   uid: string,
   new_name: string,
