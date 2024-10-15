9d99b2b0d17bf1efc940350f2df2d749314235da
tienday <develop@itutu.local>
2024-10-15 09:25:40 +0900

    improve frontend .env validation

--- frontend/src/const/API.ts
+++ frontend/src/const/API.ts
@@ -1,10 +1,13 @@
 const HOST =
   process.env.REACT_APP_SERVER_HOST || window.location.hostname || "localhost"
-const PORT = process.env.REACT_APP_SERVER_PORT || window.location.port || 8000
 const PROTO =
   process.env.REACT_APP_SERVER_PROTO ||
   window.location.protocol.replace(":", "") ||
   "http"
+const PORT =
+  process.env.REACT_APP_SERVER_PORT || window.location.port || PROTO == "https"
+    ? 443
+    : 8000
 
 export const BASE_URL =
   PORT == null ? `${PROTO}://${HOST}` : `${PROTO}://${HOST}:${PORT}`
