c5911bc007ef47c98ce90e715e598a229d5c495b
Yutaka Tsuchiyama <ytsuchiyama34@gmail.com>
2024-12-02 11:06:52 +0900

    Fixing FileSelectDialog Test Imports

--- frontend/src/components/Workspace/FlowChart/Dialog/FileSelectDialog.test.tsx
+++ frontend/src/components/Workspace/FlowChart/Dialog/FileSelectDialog.test.tsx
@@ -1,8 +1,8 @@
-import React from "react"
 import { Provider } from "react-redux"
 
 import configureStore from "redux-mock-store"
 
+import { describe, it, beforeEach } from "@jest/globals"
 import { Store, AnyAction } from "@reduxjs/toolkit"
 import { render, screen, fireEvent } from "@testing-library/react"
 
