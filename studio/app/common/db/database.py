1ae07ddbc4dd88e85e7d175b49ba51a74c387e2f
tienday <develop@itutu.local>
2024-12-16 15:12:33 +0900

    merge code diffs with optinist-for-server
    - database modules

--- studio/app/common/db/database.py
+++ studio/app/common/db/database.py
@@ -1,12 +1,30 @@
+from contextlib import contextmanager
+
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import create_engine
 
 from studio.app.common.db.config import DATABASE_CONFIG
 
-engine = create_engine(DATABASE_CONFIG.DATABASE_URL, pool_recycle=360, pool_size=20)
+engine = create_engine(
+    DATABASE_CONFIG.DATABASE_URL, pool_recycle=360, pool_size=DATABASE_CONFIG.POOL_SIZE
+)
+
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, expire_on_commit=False, bind=engine
 )
+
+
+@contextmanager
+def session_scope():
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except:  # noqa
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 def get_db():
